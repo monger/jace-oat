51aba170f259c25622c6ecb027c3726cf2f8bb56
monger <github@mongermethod.com>
2014-03-11 11:18:54 -0600

    Added default implementations of Object.hashCode(), Object.equals() and Object.toString()

--- core/runtime/src/main/java/org/jace/util/NativeInvocation.java
+++ core/runtime/src/main/java/org/jace/util/NativeInvocation.java
@@ -1,5 +1,10 @@
 package org.jace.util;
 
+import java.lang.CloneNotSupportedException;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.Throwable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -7,12 +12,28 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class NativeInvocation implements InvocationHandler {
+    private static final String EQUALS = "equals";
+    private static final String HASH_CODE = "hashCode";
+    private static final String TO_STRING = "toString";
+
     /** The class that we are proxying */
     private final Class clazz;
     
     /** A mapping of our methods to references */
     private final Map<String, Callback> callbackMap = new HashMap<String, Callback>();
-    
+
+    public static int defaultHashCode(final Object proxy) {
+        return System.identityHashCode(proxy);
+    }
+
+    public static boolean defaultEquals(final Object proxy, Object obj) {
+        return proxy.hashCode() == obj.hashCode();
+    }
+
+    public static String defaultToString(final Object proxy) {
+        return proxy.getClass().getName() + "@" + Integer.toHexString(proxy.hashCode());
+    }
+
     /**
      * Constructs a new invocation
      */
@@ -36,7 +57,16 @@ public class NativeInvocation implements InvocationHandler {
 
     @Override public Object invoke(final Object proxy, final Method method, final Object[] args) {
         if (!callbackMap.containsKey(method.getName())) {
-            throw new UnsupportedOperationException("No method " + method.getName() + " registered");
+            switch(method.getName()) {
+                case EQUALS:
+                    return defaultEquals(proxy, args[0]);
+                case HASH_CODE:
+                    return defaultHashCode(proxy);
+                case TO_STRING:
+                    return defaultToString(proxy);
+                default:
+                    throw new UnsupportedOperationException("No method " + method.getName() + " registered");
+            }
         }
         Callback c = callbackMap.get(method.getName());
         return invokeNative(c.ref, c.idx, proxy, args);
